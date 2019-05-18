3f2f885f1f17558d37b374ede0a4fbcc7933cefe
SeeligA <arndtseelig@mt-g.com>
2019-05-18 09:50:13 +0200

    docs: Add docstrings and comments for context

--- doctopic.py
+++ doctopic.py
@@ -10,14 +10,16 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 TEMP_FOLDER = tempfile.gettempdir()
 logging.info('Folder "{}" will be used to save temporary dictionary and corpus.'.format(TEMP_FOLDER))
 
-#See: https://github.com/RaRe-Technologies/gensim/wiki/Recipes-&-FAQ#q1-how-many-times-does-a-feature-with-id-123-appear-in-a-corpus
+
+# See: https://github.com/RaRe-Technologies/gensim/wiki/Recipes-&-FAQ
+#      --> Q1 How many times does a feature with id 123 appear in a corpus?
 
 
 class MyCorpus(object):
     def __init__(self, top_dir):
         self.top_dir = top_dir
         self.dictionary = corpora.Dictionary(iter_documents(top_dir))
-        self.dictionary.filter_extremes(no_below=1, keep_n=30000) # check API docs for pruning params
+        self.dictionary.filter_extremes(no_below=1, keep_n=30000)  # check API docs for pruning params
 
     def __iter__(self):
         for tokens in iter_documents(self.top_dir):
@@ -29,13 +31,25 @@ class MyCorpus(object):
         corpora.MmCorpus.serialize(os.path.join(TEMP_FOLDER, 'tmp.mm'), self)
 
     def build_tfidf(self):
-
-        tfidf = models.TfidfModel(self) # step 1 -- initialize a model
+        tfidf = models.TfidfModel(self)  # step 1 -- initialize a model
         tfidf.save(os.path.join(TEMP_FOLDER, 'tmp.tfidf'))
         return tfidf, tfidf[self]
 
     def build_lsi(self, topics=250):
+        """
+        Build LSI model from corpus and sparse word vector.
 
+        Args:
+            topics: integer value determining the number of dimensions of the LSI space
+
+        Returns:
+            tfidf: initialized tfidf model from self
+            corpus_tfidf: transformation of tfidf model and self
+            lsi: initialized lsi model from self
+            corpus_lsi: transformation of lsi model and self
+        """
+
+        # When building indeces from transformed vectors, apply same transformation to search vectors
         tfidf, corpus_tfidf = self.build_tfidf()
         lsi = models.LsiModel(corpus_tfidf, id2word=self.dictionary, num_topics=topics)
         corpus_lsi = lsi[corpus_tfidf]
@@ -48,76 +62,85 @@ class MyCorpus(object):
         tfidf.save(os.path.join(TEMP_FOLDER, 'tmp.tfidf'))
         return lda[self]
 
-
-
     def get_labels(self):
-        '''
-        Retrieve document ids with filename and directory for reference purposes
+        """
+        Retrieve document ids with filename and directory for reference purposes.
+
         Returns:
-        labels -- dict() in JSON format with integer ids mapped to list entries
-        '''
+            labels: dict() in JSON format with integer ids mapped to list entries
+        """
 
         idx = 0
         labels = {}
         for root, dirs, files in os.walk(self.top_dir):
             for file in filter(lambda file: file.endswith('.txt'), files):
                 labels[str(idx)] = [os.path.split(root)[1], file]
-                idx+=1
+                idx += 1
         # save labels to file
         save_labels(labels, os.path.join(TEMP_FOLDER, 'tmp.json'))
 
         return labels
 
     def train_lsi(self, filepath):
+        """Train new LSI model and save to TEMP."""
 
         _, tfidf_train = self.build_tfidf()
         logging.info('new tfidf corpus built')
         lsi = models.LsiModel.load(filepath)
         logging.info('existing LSI model loaded')
-        lsi.add_documents(tfidf_train) # TODO: check if there are limitations as to how many documents can be added at a time
+        lsi.add_documents(
+            tfidf_train)  # TODO: check for limitations as to how many documents can be added at a time
         logging.info('additional docs added')
         trained_lsi = lsi[tfidf_train]
         logging.info('training corpus fitted to model')
         lsi.save(os.path.join(TEMP_FOLDER, 'train.lsi'))
         return lsi, trained_lsi
 
-def build_index(corpus, num_features=False):
 
+def build_index(corpus, num_features=False):
+    """Build indices for different models.
+
+    Args:
+        corpus: either a regular MyCorpus object or a transformation
+        num_features: number of dimensions required for creating sparse vectors
+
+    Returns:
+        index: index of documents comprised in a corpus
+    """
+    # TODO: Change similarity class to Similarity()
     if num_features:
         index = similarities.SparseMatrixSimilarity(corpus, num_features=num_features)
         index.save(os.path.join(TEMP_FOLDER, 'sparse.index'))
 
     else:
-        index = similarities.MatrixSimilarity(corpus) # TODO: include option to specify num_best
+        index = similarities.MatrixSimilarity(corpus)  # TODO: include option to specify num_best
         index.save(os.path.join(TEMP_FOLDER, 'tmp.index'))
 
     return index
 
 
 def file_to_query(filepath, dictionary):
-    '''Generate BOW vector from document for querying
-    Arguments:
-    filepath -- path to document
-    dictionary -- mapping of (normalized) words to integer ids
-    '''
+    """Generate BOW vector from document for querying purposes."""
+
     if (os.path.exists(filepath)):
         document = open(filepath, 'rb').read()
         tokens = tokenize(document)
         vec_bow = dictionary.doc2bow(tokens)
         return vec_bow
 
+
 def iter_documents(top_directory):
-    '''Iterate over all documents, yielding a document (=list of utf8 tokens) at a time.'''
+    """Iterate over all documents, yielding a document (=list of utf8 tokens) at a time."""
+
     for root, dirs, files in os.walk(top_directory):
-        for file in filter(lambda file: file.endswith('.txt'), files): # TODO: Add filter for PDF files
+        for file in filter(lambda file: file.endswith('.txt'), files):  # TODO: Add filter for PDF files
             with open(os.path.join(root, file), 'rb') as document:
-                document = document.read() # read the entire document, as one big string
+                document = document.read()  # read the entire document, as one big string
                 logging.info(file)
-                yield tokenize(document) # or whatever tokenization suits you
+                yield tokenize(document)  # or whatever tokenization suits you
 
 
 def load_from_folder(folder):
-
     model, dictionary, tfidf, sparse_index, index, labels = None, None, None, None, None, None
     for file in os.listdir(folder):
         logging.info('Scanning: {}'.format(file))
@@ -138,19 +161,19 @@ def load_from_folder(folder):
     return model, dictionary, tfidf, sparse_index, index, labels
 
 
-
 def load_labels(fp):
-
     with open(fp) as f:
         labels = json.load(f)
     return labels
 
 
 def save_labels(labels, fp):
-    '''Store document ids with filename and directory for reference purposes'''
+    """Store document ids with filename and directory for reference purposes."""
+
     with open(fp, 'w') as f:
         json.dump(labels, f, sort_keys=True)
 
+
 def tokenize(document):
-    # TODO: Replace with custom tokenizer and customizable stopword list
+    # TODO: Replace with custom tokenizer and customizable stop word list
     return utils.tokenize(document, lower=True)
--- gui.py
+++ gui.py
@@ -1,26 +1,24 @@
 import sys
 
-from PyQt5 import uic
-from PyQt5.QtWidgets import qApp, QApplication, QFileDialog, QMainWindow, QWidget
-from PyQt5 import QtCore
+from qtpy import QtCore, uic
+from qtpy.QtWidgets import qApp, QApplication, QFileDialog, QMainWindow
 
-
-from PyQt5.QtGui import QIcon
 import os.path
 from doctopic import MyCorpus, load_from_folder, file_to_query, build_index
-#from GUI.restore import guisave, guirestore
+
 import logging
 import tempfile
 from shutil import copy2
 
 TEMP_FOLDER = tempfile.gettempdir()
 
+
 class MyWindow(QMainWindow):
 
     def __init__(self):
         super(MyWindow, self).__init__()
         self.initUI()
-
+        # Load settings
         self.settings = QtCore.QSettings("DocFirm Inc.", "docfind")
         # Retrieve user input from settings
         save_folder = self.settings.value("savedFolder", "")
@@ -35,52 +33,59 @@ class MyWindow(QMainWindow):
 
         uic.loadUi(os.path.join('GUI', 'doctopic.ui'), self)
         self.setWindowTitle('DocTopic')
-        self.setContentsMargins(7,0,7,0)
+        self.setContentsMargins(7, 0, 7, 0)
         self.statusBar().showMessage('Ready')
 
         self.show()
 
     def new_folder_changed(self, newFolder):
-        '''Store dirpath to settings'''
+        """Store dirpath to settings"""
         self.settings.setValue("savedFolder", w.input_line_edit_project.text())
 
     def new_doc_changed(self, newDoc):
-        '''Store filepath to settings'''
+        """Store filepath to settings"""
         self.settings.setValue("savedDoc", w.input_line_edit_doc.text())
 
     def open_project(self):
-        '''Open file dialog and write dirpath to line edit'''
+        """Open file dialog and write dirpath to line edit."""
         dirpath = QFileDialog.getExistingDirectory()
         w.input_line_edit_project.setText(dirpath)
 
     def open_doc(self):
-        '''Open file dialog and write filepath to line edit'''
-        fp = QFileDialog.getOpenFileName(filter = 'TXT-Datei (*.txt)')[0]
+        """Open file dialog and write filepath to line edit."""
+        fp = QFileDialog.getOpenFileName(filter='TXT-Datei (*.txt)')[0]
         w.input_line_edit_doc.setText(fp)
 
     def run_query(self):
-        '''
-        '''
-        # Check
+        """Run query against model."""
+
+        # Check if a model has been already loaded
         if hasattr(w, 'model'):
             pass
+        # Load relevant files from project directory
         else:
-            w.model, w.dictionary, w.tfidf, w.sparse_index, w.index, w.labels = load_from_folder(w.input_line_edit_project.text())
-
+            w.model, w.dictionary, w.tfidf, w.sparse_index, w.index, w.labels = load_from_folder(
+                w.input_line_edit_project.text())
+            # Abort query if one or more files are missing
             if all((w.model, w.dictionary, w.tfidf, w.sparse_index, w.index, w.labels)) == False:
                 logging.info('UnboundLocalError: Unable to find local variable')
                 w.textOutput.setText('UnboundLocalError: Unable to find local variable')
                 return
 
+        # Check if a query document has been specified
         if os.path.isfile(w.input_line_edit_doc.text()):
+            # Convert file to BOW vector
             vec_bow = file_to_query(w.input_line_edit_doc.text(), w.dictionary)
+            # Serialize tfidf transformation and convert search vector to LSI space
+            # Note: When using transformed search vectors, apply same transformation when building the index
             vec_lsi = w.model[w.tfidf[vec_bow]]
+            # Apply search vector to indexed LSI corpus and sort resulting index-similarity tuples.
             sims_lsi = w.index[vec_lsi]
             sims_lsi = sorted(enumerate(sims_lsi), key=lambda item: -item[1])
-
+            # Retrieve most prominent topic from search vector
             topic = w.model.print_topic(max(vec_lsi, key=lambda item: abs(item[1]))[0])
-            #doc_topics, word_topics, phi_values, = model.get_document_topics(vec_bow, per_word_topics=True)
-
+            # doc_topics, word_topics, phi_values, = model.get_document_topics(vec_bow, per_word_topics=True)
+            # Apply search vector to transformed tfidf corpus and sort resulting index-similarity tuples
             sims_tfidf = w.sparse_index[w.tfidf[vec_bow]]
             sims_tfidf = sorted(enumerate(sims_tfidf), key=lambda item: -item[1])
 
@@ -88,17 +93,9 @@ class MyWindow(QMainWindow):
         else:
             w.textOutput.setText('Please select a valid file')
 
-    def formatting_helper(secions):
-
-        len_line = 120
-        dashed = []
-        for section in sections:
-            filler = '-' * ((len_line - len(section)) / 2)
-            dashed.append(filler, section, filler)
-        return dashed
-
     def print_details(self, sims, sims_tfidf, topic):
-        '''Print query results to text output'''
+        """Print query results to text output."""
+
         sections = [' LSI Similarity ', ' Most Prominent LSI topic ', ' tf-idf Similarity ']
         line = 120
 
@@ -108,12 +105,11 @@ class MyWindow(QMainWindow):
         #
         for i in range(min(len(sims), 10)):
             labels = w.labels[str(sims[i][0])]
-            self.textOutput.append('{:<10d}{: 8.3f}\t{:>12.7}\t{:<12s}'.format(i+1, sims[i][1], labels[0], labels[1]))
+            self.textOutput.append('{:<10d}{: 8.3f}\t{:>12.7}\t{:<12s}'.format(i + 1, sims[i][1], labels[0], labels[1]))
 
         self.textOutput.append('\n')
         self.textOutput.append('{:-{align}{width}}'.format(sections[1], align='^', width=line))
         self.textOutput.append(topic)
-
 
         self.textOutput.append('\n')
         self.textOutput.append('{:-{align}{width}}'.format(sections[2], align='^', width=line))
@@ -122,16 +118,15 @@ class MyWindow(QMainWindow):
 
         for i in range(min(len(sims_tfidf), 10)):
             labels = w.labels[str(sims_tfidf[i][0])]
-            self.textOutput.append('{:<10d}{: 8.3f}\t{:>12.7}\t{:<12s}'.format(i+1, sims_tfidf[i][1], labels[0], labels[1]))
-
-
+            self.textOutput.append(
+                '{:<10d}{: 8.3f}\t{:>12.7}\t{:<12s}'.format(i + 1, sims_tfidf[i][1], labels[0], labels[1]))
 
     def reset_model(self):
-        '''Delete attribute when changing the project folder'''
+        """Delete attribute when changing the project folder."""
+
         logging.info('Reset model')
         if hasattr(w, 'model'):
             del w.model
-
 
     def train_model(self):
         if os.path.isdir(w.input_line_edit_doc.text()):
@@ -149,7 +144,8 @@ class MyWindow(QMainWindow):
 
     def save_model(self):
         src = TEMP_FOLDER
-        dst = w.input_line_edit_project.text()
+        dst = w.input_line_edit_project.text()  # TODO: Check if the path is a valid folder
+        # list of files expected in your TEMP folder after training a model
         files = ['sparse.index',
                  'tmp.index',
                  'tmp.lsi',
@@ -164,7 +160,6 @@ class MyWindow(QMainWindow):
 
 
 if __name__ == '__main__':
-
     app = QApplication(sys.argv)
     w = MyWindow()
     w.open_project_button.clicked.connect(MyWindow.open_project)
@@ -175,6 +170,5 @@ if __name__ == '__main__':
     w.input_line_edit_project.textChanged.connect(MyWindow.reset_model)
     w.train_model_button.clicked.connect(MyWindow.train_model)
     w.save_project_button.clicked.connect(MyWindow.save_model)
-
 
     sys.exit(app.exec_())
